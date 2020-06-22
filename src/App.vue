0db2191cab0de47c745ed1e646591583536f02cc
raw-coder <singular1.steve@gmail.com>
2020-06-23 08:42:26 +0900

    add Navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    {{msg}}
+    <Navbar></Navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar';
+
 export default {
+  components : {
+    Navbar,
+  },
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
 }
 </script>
 
