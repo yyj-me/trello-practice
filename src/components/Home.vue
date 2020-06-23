3eb7036de13a3477071f36a7f66e5d9b0205762f
raw-coder <singular1.steve@gmail.com>
2020-06-24 08:30:11 +0900

    add api call sample by using XMLHttpRequest

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,10 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        Api result: {{apiRes}}
+      </div>
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -17,7 +21,34 @@
 
 <script>
   export default {
-    name: "Home.vue"
+    name: "Home.vue",
+    data() {
+      return {
+        loading: false,
+        apiRes: '',
+      };
+    },
+    created() {
+      this.fetchData();
+    },
+    methods: {
+      fetchData() {
+        this.loading = true;
+
+        const req = new XMLHttpRequest();
+        req.open('GET', 'http://localhost:3000/health');
+        req.send();
+
+        req.addEventListener('load', () => {
+          this.loading = false;
+          this.apiRes = {
+            status: req.status,
+            statusText: req.statusText,
+            response: JSON.parse(req.response),
+          };
+        });
+      }
+    }
   }
 </script>
 
