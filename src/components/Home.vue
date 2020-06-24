f107358b42983dc3e47d60de1b5a5fa941c38a39
raw-coder <singular1.steve@gmail.com>
2020-06-25 08:57:06 +0900

    modify Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
-    Home
-    <div>
-      Board List:
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-for="b in boards" :key="b.id">{{b}}</div>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list">
+      <div class="board-item" v-for="b in boards" :key="b.id" :data-bgcolor="b.bgColor" ref="boardItem">
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{b.title}}</div>
+        </router-link>
       </div>
-      <ul>
-        <li>
-          <router-link to="/b/1">Board 1</router-link>
-        </li>
-        <li>
-          <router-link to="/b/2">Board 2</router-link>
-        </li>
-      </ul>
+      <div class="board-item">
+        <a class="new-board-btn" href="" @click.prevent="addBoard">
+          Create new board...
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -28,10 +25,16 @@
       return {
         loading: false,
         boards: [],
+        error: '',
       };
     },
     created() {
       this.fetchData();
+    },
+    updated() {
+      this.$refs.boardItem.forEach(el => {
+        el.style.backgroundColor = el.dataset.bgcolor;
+      })
     },
     methods: {
       fetchData() {
@@ -43,11 +46,55 @@
         .finally(() => {
           this.loading = false;
         })
+      },
+      addBoard() {
+        console.log('addBoard');
       }
     }
   }
 </script>
 
 <style scoped>
-
+  .home-title {
+    padding: 10px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .board-list {
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .board-item {
+    width: 23%;
+    height: 100px;
+    margin: 0 2% 20px 0;
+    border-radius: 3px;
+  }
+  .board-item a {
+    text-decoration: none;
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+  .board-item a:hover,
+  .board-item a:focus {
+    background-color: rgba(0,0,0, .1);
+    color: #666;
+  }
+  .board-item-title {
+    color: #fff;
+    font-size: 18px;
+    font-weight: 700;
+    padding: 10px;
+  }
+  .board-item a.new-board-btn {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    height: 100px;
+    width: inherit;
+    color: #888;
+    font-weight: 700;
+  }
 </style>
